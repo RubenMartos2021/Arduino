fb73d460cf726c65164a8ae67cb0de10b30a1e52
RubenMartos7 <ruben.martos.2144@lacetania.cat>
2021-11-26 16:44:16 +0100

    Exercici 7

---
+++ Tipus de Dades i Operadors/Exercici_7/Exercici_7.ino
@@ -0,0 +1,38 @@
+/**********************************************************************
+**                                                                   **
+**                               TÍTOL:                              **
+**                      LL01_Rubén_Martos_ARDUINO                    **
+**                                                                   **
+**  NOM: Rubén Martos Gutiérrez                     Data: 18/11/2021 **
+**********************************************************************/
+//************************** INCLUDE **********************************
+
+
+//************************ VARIABLES **********************************
+
+int drive_gb = 100;
+int drive_mb = 0;
+
+//************************** SETUP ************************************
+
+void setup()  // run once when the sketch starts
+{
+  Serial.begin(9600);  // set up Serial library at 9600 bps
+  Serial.print("Your HD is ");
+  Serial.print(drive_gb);
+  Serial.println(" GB large. ");
+
+  drive_mb = 1024 * drive_gb;
+
+  Serial.print("It can store ");
+  Serial.print(drive_mb);
+  Serial.println(" Megabytes!");
+}
+
+//************************** LOOP *************************************
+
+void loop()  // we need this to be here even though its empty
+{                 
+}
+
+//************************** FUNCIONS **********************************
