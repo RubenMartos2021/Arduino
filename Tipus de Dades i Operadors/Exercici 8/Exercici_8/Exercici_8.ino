66d654e652c574c05c67d3eb00b667c2a7b26ffa
RubenMartos7 <ruben.martos.2144@lacetania.cat>
2021-11-27 12:34:20 +0100

    Exercici 8

---
+++ Tipus de Dades i Operadors/Exercici 8/Exercici_8/Exercici_8.ino
@@ -0,0 +1,39 @@
+/**********************************************************************
+**                                                                   **
+**                               TÍTOL:                              **
+**                      LL03_Rubén_Martos_ARDUINO                    **
+**                                                                   **
+**  NOM: Rubén Martos Gutiérrez                     Data: 27/11/2021 **
+**********************************************************************/
+//************************** INCLUDE **********************************
+
+
+//************************ VARIABLES **********************************
+
+int drive_gb = 100;
+long drive_mb;  // he canviat de tipus de dada
+
+//************************** SETUP ************************************
+
+void setup()  // ho fa un cop quan el programa s'inicia
+{
+  Serial.begin(9600);  
+  
+  Serial.print("Your HD is ");
+  Serial.print(drive_gb);
+  Serial.println(" GB large. ");
+
+  drive_mb = 1024 * drive_gb;
+
+  Serial.print("It can store ");
+  Serial.print(drive_mb);
+  Serial.println(" Megabytes!");
+}
+
+//************************** LOOP *************************************
+
+void loop()  // we need this to be here even though its empty
+{
+}
+
+//************************** FUNCIONS  *********************************
