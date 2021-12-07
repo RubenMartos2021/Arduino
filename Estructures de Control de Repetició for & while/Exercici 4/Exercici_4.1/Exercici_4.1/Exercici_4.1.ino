7098d5029be6ff1aac15b69320ff37c54b55b9ed
RubenMartos7 <ruben.martos.2144@lacetania.cat>
2021-12-07 11:19:33 +0100

    Exercici 4.1

---
+++ Estructures de Control de Repetició for & while/Exercici 4/Exercici_4.1/Exercici_4.1/Exercici_4.1.ino
@@ -0,0 +1,37 @@
+/**********************************************************************
+**                                                                   **
+**                               TÍTOL:                              **
+**                      LL05_Rubén_Martos_ARDUINO                    **
+**                                                                   **
+**  NOM: Rubén Martos Gutiérrez                     Data: 04/12/2021 **
+**********************************************************************/
+//************************** INCLUDE **********************************
+
+
+//************************ VARIABLES **********************************
+int taula = 7;
+
+//************************** SETUP ************************************
+
+void setup()  // ho fa un cop quan el programa s'inicia
+{
+  Serial.begin(9600);
+  Serial.print("Taula de multiplicar del ");
+  Serial.println(taula);
+  for (int i=0; i <= 10; i++)
+  {
+    delay(100);
+    Serial.print(taula);
+    Serial.print(" x ");
+    Serial.print(i);
+    Serial.print(" = ");
+    Serial.println(taula*i);
+  }
+}
+
+//************************** LOOP *************************************
+void loop()      //we need this to be here even though its empty
+{
+}
+
+//************************** FUNCIONS  *********************************
