20a4078b6700a60735c739f8ff57d41cf22eac00
RubenMartos7 <ruben.martos.2144@lacetania.cat>
2021-12-17 11:28:55 +0100

    Exercici 7

---
+++ Array de 8 LEDs/Exercici 7/Exercici_7/Exercici_7.ino
@@ -0,0 +1,175 @@
+/**********************************************************************
+**                                                                   **
+**                               TÍTOL:                              **
+**                      LL06_Rubén_Martos_ARDUINO                    **
+**                                                                   **
+**  NOM: Rubén Martos Gutiérrez                     Data: 15/12/2021 **
+**********************************************************************/
+//************************** INCLUDE **********************************
+
+
+//************************ VARIABLES **********************************
+const byte led3 = 8;          // donar nom al pin 8 de l’Arduino
+//************************** SETUP ************************************
+
+void setup()  // ho fa un cop quan el programa s'inicia
+{
+  pinMode(led3, OUTPUT);     // definir el pin 7 com una sortida
+
+   digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+
+ digitalWrite(led3, HIGH);
+ delay (300);
+  digitalWrite(led3, LOW);
+ delay (300);
+}
+
+//************************** LOOP *************************************
+void loop()      //we need this to be here even though its empty
+{
+}
+
+//************************** FUNCIONS  *********************************
