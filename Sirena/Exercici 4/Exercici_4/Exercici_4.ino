c9fbf72fc43963a813bdc89a651dd93542fc0a81
RubenMartos7 <ruben.martos.2144@lacetania.cat>
2022-01-14 10:59:55 +0100

    Exercici 4

---
+++ Sirena/Exercici 4/Exercici_4/Exercici_4.ino
@@ -0,0 +1,38 @@
+/**********************************************************************
+**                                                                   **
+**                               TÍTOL:                              **
+**                      LL09_Rubén_Martos_ARDUINO                    **
+**                                                                   **
+**  NOM: Rubén Martos Gutiérrez                     Data: 12/01/2022 **
+**********************************************************************/
+//************************** INCLUDE **********************************
+
+
+//************************ VARIABLES **********************************
+const byte bitonal = 9;        // donar nom al pin 9 de l’Arduino
+const byte pot0 = A0;         // donar nom al pin A0 de l’Arduino
+int valPot0;                 // guardar valor del potenciometre  
+
+
+//************************** SETUP ************************************
+
+void setup()  // ho fa un cop quan el programa s'inicia
+{
+ pinMode(bitonal, OUTPUT);   // definir el pin 9 com una sortida
+
+}
+
+//************************** LOOP *************************************
+void loop()      //we need this to be here even though its empty
+{
+  valPot0 = analogRead(pot0);    // llegir valor potenciòmetre 
+
+  tone(bitonal, 1000, 20);    // xiulet de durada valPot0
+  delay(2*valPot0);              // esperar valPot0 del xiulet + valPot0 silenci  
+  
+   valPot0 = analogRead(pot0);    // llegir valor potenciòmetre
+  tone(bitonal, 1250, 20);    // xiulet de durada valPot0
+  delay(2*valPot0);              // esperar valPot0 del xiulet + valPot0 silenci             
+}
+
+//************************** FUNCIONS  *********************************
